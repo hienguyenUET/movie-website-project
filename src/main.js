c88ac6d64bcf5c456e2a76f9f607bce9d351ec12
asdfghjklmnbvcz <90941901+lak-glitch@users.noreply.github.com>
2022-03-23 00:04:23 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/js/bootstrap.js"
+const app = createApp(App);
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+app.use(VueAxios, axios)
+import router from './router'
+app.use(router);
+import SignUp from './components/SignUp.vue'
+import LoginForm from './components/LoginForm.vue'
+app.component('sign-up', SignUp)
+app.component('login-form', LoginForm)
+app.mount('#app')
