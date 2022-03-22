caaab8091315f80344008bb6c1324b5b23257c1f
asdfghjklmnbvcz <90941901+lak-glitch@users.noreply.github.com>
2022-03-23 00:03:43 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  methods: {},
+};
+</script>
+
+<style>
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease-out;;
+}
+</style>
